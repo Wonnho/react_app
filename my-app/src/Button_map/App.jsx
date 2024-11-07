87a8cb36a208fca85beec4c29a09366e5b2c8b9a
Choi Wonnho <wonnho@nate.com>
2024-11-08 08:49:09 +0900

    create Button via map

---
+++ my-app/src/Button_map/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ButtonContainer from "./ButtonContainer";
+function App() {
+  return(
+    <>
+    <ButtonContainer></ButtonContainer>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/Button_map/ButtonContainer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Button from "./Button";
+export default function ButtonContainer() {
+  const buttonColor = ["blue", "red", "yellow"];
+  const buttonText = ["확인", "취소", "보류", "1억년"];
+
+  const buttonData = [
+    { backgroundColor: "blue", text: "확인" },
+    { backgroundColor: "red", text: "취소" },
+    { backgroundColor: "gred", text: "보류" },
+    { backgroundColor: "pink", text: "1억년" },
+  ];
+  const button = buttonData.map((btn) => {
+    const { backgroundColor, text } = btn;
+    return <Button backgroundColor={backgroundColor}>{text}</Button>;
+  });
+
+  return (
+    <div>
+      {/* <Button backgroundColor="blue" >확인</Button>
+      <Button backgroundColor="blue" >취소</Button>
+      <Button backgroundColor="blue" >보류</Button>
+      <Button backgroundColor="blue">1억년</Button> */}
+      {button}
+    </div>
+  );
+}
